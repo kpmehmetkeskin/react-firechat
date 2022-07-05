b5f0be3c1cc2ce44ece07c22313682a304628fa0
kpmehmetkeskin <cs.mehmetkeskin@gmail.com>
2022-07-05 23:12:32 +0300

    integration

--- src/App.js
+++ src/App.js
@@ -10,12 +10,13 @@ import Channel from './components/Channel';
 import Loader from './components/Loader';
 
 firebase.initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_AUTH_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyAlwgnETafa0JFgAWYfbPnEe1BycLlF5a8",
+  authDomain: "wmapchat.firebaseapp.com",
+  projectId: "wmapchat",
+  storageBucket: "wmapchat.appspot.com",
+  messagingSenderId: "623997961309",
+  appId: "1:623997961309:web:92de103b097aa4c36e39b4",
+  measurementId: "G-0SY71RB1Q2"
 });
 
 const MoonIcon = props => (
