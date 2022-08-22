730448cdcc9ba3c635f29084943247a9b89cdbfd
kpmehmetkeskin <cs.mehmetkeskin@gmail.com>
2022-08-22 22:14:36 +0300

    Sadelestirme

--- src/components/Message.js
+++ src/components/Message.js
@@ -24,15 +24,6 @@ const Message = ({
 
   return (
     <div className="px-4 py-4 rounded-md hover:bg-gray-50 dark:hover:bg-coolDark-600 overflow-hidden flex items-start">
-      {photoURL ? (
-        <img
-          src={photoURL}
-          alt="Avatar"
-          className="rounded-full mr-4"
-          width={45}
-          height={45}
-        />
-      ) : null}
       <div>
         <div className="flex items-center mb-1">
           {displayName ? (
